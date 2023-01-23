73ab1743989397869a74be5c0b70040303b5441b
Leonid Holub <rsleonid@gmail.com>
2023-01-23 10:02:08 +0200

    HW done

---
+++ controllers/auth/index.js
@@ -0,0 +1,15 @@
+const {
+  signup,
+  login,
+  getCurrent,
+  logout,
+  resendVerifyEmail,
+} = require("./auth");
+
+module.exports = {
+  signup,
+  login,
+  getCurrent,
+  logout,
+  resendVerifyEmail,
+};
--- helpers/index.js
+++ helpers/index.js
@@ -1,7 +1,11 @@
 const ctrlWrapper = require("./ctrlWrapper");
 const HandleMongooseError = require("./handleMongooseError");
+const HttpError = require("./HttpError");
+const sendEmail = require("./sendEmail");
 
 module.exports = {
   ctrlWrapper,
   HandleMongooseError,
+  HttpError,
+  sendEmail,
 };
